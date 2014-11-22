a58d71b26b4cea318fba8908ffe2bd98cce82535
NagNag <nagesh.nagara@gmail.com>
2014-11-22 23:02:21 +0000

    Modified cacheSolve.R as part of Prog Ass 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+##Given a matrix, this function simply constructs a cache of values related to the passed matrix, as a list.
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL # set value of m to NULL
+      y <- NULL # set value of y to NULL
+      setmat <- function(y) {
+            x <<- y # set the value of x which is outside the scope of this function, hence <<-
+            m <<- NULL # set the value of m which is outside the scope of this function, hence <<-
+      }
+      getmat <- function() x
+      setinv <- function(invX) m <<- invX
+      getinv <- function() m
+      list(set = setmat, get = getmat, # creates a list to store the four functions
+           setinv = setinv,
+           getinv = getinv)
+      
 }
 
 
-## Write a short comment describing this function
+## This function gets the inverse of the matrix returned by makeCacheMatrix defined above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      # Compare given matrix to the previous
+      m <- x$getinv()
+      y <- x$get() # get value of input matrix
+      
+      ## check if cached before
+      if(!is.null(m) ){
+            message("Already cached")
+            return(m)
+            #
+      }       
+      else
+      {
+            # if not cached before  
+            message("Not cached so will perform expensive solve function here")
+            m <- solve(y) # get inverse of the given matrx
+            x$setinv(m) # cache the inverse
+            m # return the inverse
+      }
 }
+
+
